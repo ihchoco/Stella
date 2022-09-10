a0daa80160c9ecd31ac4a1df910dc5eef101ff74
giyeon <qkrrldus93g@gmail.com>
2022-09-10 20:32:49 +0900

    Feat : Add Slf4j loggin

--- src/main/java/com/example/stella/controller/ArticleController.java
+++ src/main/java/com/example/stella/controller/ArticleController.java
@@ -3,34 +3,40 @@ package com.example.stella.controller;
 import com.example.stella.controller.repository.ArticleRepository;
 import com.example.stella.dto.ArticleForm;
 import com.example.stella.entity.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 위한 어노테이션
 public class ArticleController {
-
     @Autowired // 스프링 부트가 미리 생성해놓은 객체를가져다가 자동 연결
     private ArticleRepository articleRepository;
-
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
     }
-
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        //실제 서버에서는 절대로 System.out.println()을 사용하면 안된다.
+        //1. 확인도 안되고 2. 서버에 부하도 많이 준다.
+        //System.out.println( -> 로깅 기능으로 대체 필요
+        //Logging이란? => 서버에서 일어나는 모든일을 기록하는 것
+        //System.out.println(form.toString());
+        log.info(form.toString()); //System.out.println(form.toString) => log.info(form.toString)
 
         // 1. Dto를 변환! Entity!
         // DTO는 자바의 객체, DB에 저장하기 위해서는ㄴ Entity로 변환이 필요하고 그 다음 Repository를 통해서 DB에 저장
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString()); //System.out.println(article.toString) => log.info(article.toString)
 
         // 2. Repository에게 Entity를 DB안에 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/stella/entity/Article.java
+++ src/main/java/com/example/stella/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.stella.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // 여기에 @Entity 어노테이션을 달아줘야지 DB에서 인식해서 저장이 가능하
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id // 대표값을 지정! Like a 주민등록번호
@@ -18,18 +23,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
